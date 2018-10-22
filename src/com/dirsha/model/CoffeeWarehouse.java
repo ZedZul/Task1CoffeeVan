09e2dcaa2bbc7c27bf59c0b4eb2570879d864b93
Vadim Dirsha <faqproshival@yandex.ru>
2018-10-22 20:21:26 +0300

    add primitive structure

---
+++ src/com/dirsha/model/CoffeeWarehouse.java
@@ -0,0 +1,11 @@
+package com.dirsha.model;
+
+import com.dirsha.interfaces.INomenclature;
+
+/**
+ @author = Vadim Dirsha
+ @date = 20.10.2018
+ */
+public class CoffeeWarehouse implements INomenclature
+{
+}
